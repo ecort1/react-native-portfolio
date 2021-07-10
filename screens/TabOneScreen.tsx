8ae093929f353a943c73e3adffd6ccf81b424695
ecort1 <mrericcort@gmail.com>
2021-07-10 09:19:37 -0400

    Add files via upload

---
+++ screens/TabOneScreen.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { StyleSheet, Image} from 'react-native';
+import { Text, View } from '../components/Themed';
+//<Image source={require('./siteequal-logo.png')} />
+//<Image source={require('./ucann2-logo.jpg')} />
+//<Image source={require('./mosimos-systems.jpg')} />
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Portfolio</Text>
+      <Image source={require('./siteequal-logo.png')} />
+      <Image source={require('./ucann2-logo.jpg')} />
+      <Image source={require('./mosimos-systems.jpg')} />
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
